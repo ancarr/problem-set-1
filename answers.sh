e1837739d11865d3e6595d77ffe40e29ce778595
Rachel Ancar <rachel.ancar@ucdenver.edu>
2017-02-02 13:34:15 -0700

    answer 1-2

--- answers.sh
+++ answers.sh
@@ -1,17 +1,27 @@
 #! /usr/bin/env bash 
 
-datasets='/Users/jayhesselberth/devel/MOLB7621/data-sets'
-# Which state has the highest popultion?
+## Question 1: Which state in 'states.tab.gz' has the lowest murder rate
 
-states="$datasets/states.tab.gz"
+states=$HOME/projects/data-sets/misc/states.tab.gz  
 
 answer_1=$(gzcat $states \
     | grep -v '^#' \
-    | cut -f1,2 \
+    | cut -f1,6 \
     | sort -k2n \
-    | tail -n1 \
+    | head -n1 \
     | cut -f1 \
     | sed 's/"//g') 
 
 echo "answer-1: $answer_1"
+
+## Question 2: How many sequence records are in the sample.fa file?
+
+fa=~/projects/data-sets/fasta/sample.fa
+
+answer_2=$(cat $fa \
+    | grep -v '^>' \
+    | wc -l) 
+
+echo "answer-2: $answer_2" 
+
 
